d7bbc72b1e3424aed562eb7131708ce64f0a83a9
anbgd <47751238+anbgd@users.noreply.github.com>
2020-03-31 15:30:33 +0300

    0*0

--- der2.m
+++ der2.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = der2(p)
+function der2(p)
 [m,n] = size(p);
 derx = zeros(m,n-1);
 dery = zeros(m-1,n);
@@ -10,6 +10,6 @@ for i = 1:m
     stl = p(:,i);    
     dery(:,i) = derivative(stl);
 end
-outputArg1 = derx;
-outputArg2 = dery;
+derx
+dery
 end
